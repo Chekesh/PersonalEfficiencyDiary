61090c010d4d7f5a0e2bf695f10183b24a5d63e7
Chekesh <122931647+Chekesh@users.noreply.github.com>
2023-11-07 15:21:37 +0300

    dopil

--- PersonalEfficiencyDiary/app/src/main/java/com/example/personalefficiencydiary/DataBaseAccessor.java
+++ PersonalEfficiencyDiary/app/src/main/java/com/example/personalefficiencydiary/DataBaseAccessor.java
@@ -12,32 +12,84 @@ import java.io.File;
 
 public class DataBaseAccessor extends SQLiteOpenHelper
 {
+    // Основные данные базы
     private static final String DATABASE_NAME = "db.db";
     private static final int DB_VERSION = 3;
 
-    private static final String TABLE_NOTE = "NOTE";
+    // таблицы
+    private static final String TABLE_RECORDS = "RECORDS";
 
-    private static final String COLUMN_ID = "_id";
-    private static final String COLUMN_TIME = "time";
-    private static final String COLUMN_RATING = "rating";
-    private static final String COLUMN_COUNT = "count";
-    private static final String COLUMN_COMM = "comment";
+    // столбцы таблицы RECORDS
+    private static final String RECORDS_ID = "id";
+    private static final String RECORDS_DATE = "date";
+    private static final String RECORDS_RATING = "rating";
+    private static final String RECORDS_EVALUATION = "Evaluation";
+    private static final String RECORDS_HOW_MACH = "HowMuch";
+    private static final String RECORDS_HOW_MATH_NOT = "HowMuchNot";
+    private static final String RECORDS_COMENT = "Coment";
 
     public DataBaseAccessor(Context context) {
-        super(context, TABLE_NOTE, null, DB_VERSION);
+        super(context, DATABASE_NAME, null, DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE_NOTE + "("
-                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_TIME + " TEXT,"
-                + COLUMN_RATING + " TEXT,"
-                + COLUMN_COUNT);
+        db.execSQL("CREATE TABLE " + TABLE_RECORDS + "("
+                + RECORDS_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + RECORDS_DATE + " TEXT,"
+                + RECORDS_RATING + " TEXT,"
+                + RECORDS_EVALUATION + " TEXT,"
+                + RECORDS_HOW_MACH + " TEXT,"
+                + RECORDS_HOW_MATH_NOT + " TEXT,"
+                + RECORDS_COMENT + " TEXT);");
+        db.execSQL("INSERT INTO " + TABLE_RECORDS + " values(1,'2023-09-30','5','10','5','2','Сегодня был день рождения моего брата')");
+        db.execSQL("INSERT INTO " + TABLE_RECORDS + " values(2,'2023-10-09','2','4','2','5','Сегодня я впал в дипресиию')");
+        db.execSQL("INSERT INTO " + TABLE_RECORDS + " values(3,'2023-10-15','5','10','2','0','Сегодня я вышел из дипресии')");
+        db.execSQL("INSERT INTO " + TABLE_RECORDS + " values(4,'2023-10-20','4','7','3','3','Среднестатистический день')");
+    }
+
+    public void updateNote(int id, String date,String rating, String evaluation,String howmach, String howmachnor,String coment)
+    {
+        // выполнить запрос на обновление БД
+        getReadableDatabase().execSQL("UPDATE "+ TABLE_RECORDS
+                + " SET "
+                + RECORDS_DATE + "='" + date + "', "
+                + RECORDS_RATING + "='" + rating + "', "
+                + RECORDS_EVALUATION + "='" + evaluation + "', "
+                + RECORDS_HOW_MACH + "='" + howmach + "', "
+                + RECORDS_HOW_MATH_NOT + "='" + howmachnor + "', "
+                + RECORDS_COMENT + "='" + coment + "'"
+                + " WHERE "
+                + RECORDS_ID + "=" + id);
+    }
+
+    public SimpleCursorAdapter getCursorAdapter(Context context, int layout, int[] viewIds)
+    {
+        // запрос данных для отображения
+        Cursor cursor = getReadableDatabase().rawQuery("SELECT * FROM " + TABLE_RECORDS,null);
+
+        // какие столбцы и в каком порядке показывать в listview
+        String[] columns = new  String[] {RECORDS_DATE,RECORDS_RATING};
+
+        // создание адаптера
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(context,layout,cursor,columns,viewIds,0);
+        return  adapter;
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try
+        {
+            //удалить старую таблицу
+            db.execSQL("DROP TABLE " + TABLE_RECORDS);
+        }
+        catch (Exception exception)
+        {
 
+        }
+        finally {
+            //создать новую и заполнить ее
+            onCreate(db);
+        }
     }
 }
--- PersonalEfficiencyDiary/app/src/main/java/com/example/personalefficiencydiary/MainActivity.java
+++ PersonalEfficiencyDiary/app/src/main/java/com/example/personalefficiencydiary/MainActivity.java
@@ -1,12 +1,31 @@
 package com.example.personalefficiencydiary;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
+    final public static String KEY_DATE = "Date";
+    final public static String KEY_RATING = "Rating";
+    final public static String KEY_EVALUATION = "Evaluation";
+    final public static String KEY_HOW_MACH = "HowMuch";
+    final public static String KEY_HOW_MATH_NOT = "HowMuchNot";
+    final public static String KEY_COMENT = "Coment";
+
+    ListView ThemesListView;
+
+    SimpleCursorAdapter noteAdapter;
     DataBaseAccessor db;
 
     @Override
@@ -16,7 +35,39 @@ public class MainActivity extends AppCompatActivity {
 
         Context context = this;
 
+        noteAdapter = AdapterUpdate();
+
         db = new DataBaseAccessor(context);
     }
+
+    private SimpleCursorAdapter AdapterUpdate() {
+        // получить адаптер из класса
+        SimpleCursorAdapter adapter = db.getCursorAdapter(this,
+                android.R.layout.two_line_list_item, // Разметка одного элемента ListView
+                new int[]{android.R.id.text1,android.R.id.text2}); // текст этого элемента
+
+        // установить адаптер в listview
+        ThemesListView.setAdapter(adapter);
+        return adapter;
+    }
+
+    ActivityResultLauncher<Intent> NotesLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    // все ли хорошо при получении данных из дочерней активити?
+                    if(result.getResultCode() == Activity.RESULT_OK) {
+                        Intent intent = result.getData();
+                        String date = intent.getStringExtra(KEY_DATE);
+                        String rating = intent.getStringExtra(KEY_RATING);
+                        String Evaluation = intent.getStringExtra(KEY_EVALUATION);
+                        String HowMuch = intent.getStringExtra(KEY_HOW_MACH);
+                        String HowMuchNot = intent.getStringExtra(KEY_HOW_MATH_NOT);
+                        String Coment = intent.getStringExtra(KEY_COMENT);
+                    }
+                }
+            });
+
 }
 
